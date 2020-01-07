fcc1f15533c33b69ae3bbb8a49f7250c088b36c3
Joe Hancock <joe.hancock95@gmail.com>
2020-01-07 13:45:52 -0600

    added frames

--- assets/app.js
+++ assets/app.js
@@ -1,16 +1,29 @@
-onload="startGame()"
+// onload="startGame()"
 
-function startGame() {
-    myGameArea.start();
-}
+// var myGamePiece;
 
-var myGameArea = {
-    canvas : document.createElement("canvas"),
-    start : function() {
-        this.canvas.width = 480;
-        this.canvas.height = 270;
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas,
-            document.body.childNodes[0]);
-    }
-}
+// function startGame() {
+//     myGameArea.start();
+//     myGamePiece = new component(30, 30, "black", 10, 120);
+// }
+// // canvas element
+// var myGameArea = {
+//     canvas : document.createElement("canvas"),
+//     start : function() {
+//         this.canvas.width = 480;
+//         this.canvas.height = 270;
+//         this.context = this.canvas.getContext("2d");
+//         document.body.insertBefore(this.canvas,
+//             document.body.childNodes[0]);
+//     }
+// }
+
+// function component(width, height, color, x, y) {
+//     this.width = width;
+//     this.height = height;
+//     this.x = x;
+//     this.y = y;
+//     ctx = myGameArea.context;
+//     ctx.fillStyle = color;
+//     ctx.fillRect(this.x, this.y, this.width, this.height);
+// }
